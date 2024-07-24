37a1fe235c8148f6e1614fe546102c5fbeb64289
maikynata <maikynata25@gmail.com>
2024-07-24 19:27:34 +0200

    routes to record

---
+++ server/routes/record.js
@@ -0,0 +1,21 @@
+import express from "express";
+import db from "../db/connection"
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    let collection = await db.collection("records");
+    let results = await collection.find({}).toArray();
+    res.send(results).status(200);
+})
+
+router.get("/:id", async (req, res) => {
+    let collection = await db.collection("records");
+    let query = { _id: new ObjectId(req.params.id) };
+    let result = await collection.findOne(query);
+
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+})
+
