d7dc896c0a749053282ab0201525a44d217b844c
maikynata <maikynata25@gmail.com>
2024-07-29 19:48:08 +0200

    installing react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import Record from "./components/Record";
+import RecordList from "./components/RecordList";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <RecordList/>,
+      },
+    ],
+  },
+  
+])
